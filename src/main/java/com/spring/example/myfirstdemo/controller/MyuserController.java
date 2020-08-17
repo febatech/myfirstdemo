20a5b4adc33c2b432b0132420bdcf90ba4d02753
vponnap <getvenkathere@gmail.com>
2020-08-17 20:31:48 +0530

    this is my second commit

---
+++ src/main/java/com/spring/example/myfirstdemo/controller/MyuserController.java
@@ -0,0 +1,35 @@
+package com.spring.example.myfirstdemo.controller;
+
+import com.spring.example.myfirstdemo.domain.details;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.ArrayList;
+@RestController
+public class MyuserController {
+
+    @GetMapping("/hellodocker")
+    public String getNames() {
+        return ( "Welcome to Dockers and K8S, Jenkins ");
+    }
+
+    @GetMapping("/hellokanha")
+    public String getKanha() {
+        return ( "Welcome to Kanha and be safe ");
+    }
+
+    @GetMapping("/getkanhadetails")
+    public List<details> getKanhaDetails() {
+        List<details> kanhadetails = new ArrayList<>();
+         details d1 = new details( "THLC" , "School");
+         details d2 = new details( "Kaivalya" , "Apartment");
+         details d3 = new details( "Meera" , "Houses");
+         kanhadetails.add(d1);
+         kanhadetails.add(d2);
+        kanhadetails.add(d3);
+         return kanhadetails;
+
+    }
+
+}
